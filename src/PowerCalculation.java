91c66b8dd9202fd5bde3ec02239ac545cbe2b10c
john <johnludus1977@example.com>
2026-04-29 18:01:24 +0530

    very easy(using MathFun)

---
+++ src/PowerCalculation.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class PowerCalculation {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        float num1 = sc.nextFloat();
+        float num2 = sc.nextFloat();
+        double  power = Math.pow(num1,num2);
+        System.out.println(power);
+
+
+    }
+
+}
