a116445fe0efe4d812c551ac398c387e49302e16
john <johnludus1977@example.com>
2026-04-29 17:35:50 +0530

    very easy(using MathFun)

---
+++ src/SquareRoot.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class SquareRoot {
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+    double sqRoot = Math.sqrt(num);
+    System.out.println(sqRoot);
+
+    }
+
+}
