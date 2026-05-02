5225af8f0a7f2a3a6569f5edc6c1003bee97e048
john <johnludus1977@example.com>
2026-05-03 00:25:46 +0530

    very easy(Terinary operator)

---
+++ src/GreatestOfThree.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class GreatestOfThree {
+    public static void main(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the numbers:");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println("The greatest number is : "+((a>b)?(a>c?a:c):(b>c?b:c)));
+
+    }
+
+}
