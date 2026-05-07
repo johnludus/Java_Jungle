f04332cd43365901e693c745cfd0e214bb078d46
john <johnludus1977@example.com>
2026-05-07 18:05:58 +0530

    very easy(MathFun)

---
+++ src/VolumeOfSphere.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class VolumeOfSphere {
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int radius = sc.nextInt();
+    double volume = (4.0/ 3) * (Math.PI) * Math.pow(radius, 3);
+    System.out.println("Volume of a Sphere is : "+volume);
+}
+}
