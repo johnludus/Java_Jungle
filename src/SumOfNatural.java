3078c0eb3e42f2828b6f616e41cb843465407d08
john <johnludus1977@example.com>
2026-05-19 18:06:13 +0530

    very easy(using for loop)

---
+++ src/SumOfNatural.java
@@ -0,0 +1,11 @@
+public class SumOfNatural {
+    public static void main(String[] args){
+        int n=50;
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            sum+=i;
+        }
+        System.out.println(sum);
+    }
+
+}
