7bb24f8b7c67b681c847e9617a38afa6b2c1f19b
john <johnludus1977@example.com>
2026-05-19 18:06:32 +0530

    very easy(using while loop)

---
+++ src/SumOfNatural02.java
@@ -0,0 +1,10 @@
+public class SumOfNatural02 {
+    public static void main(String[] args){
+        int nos=1,sum=0,upto=50;
+        while(nos<=upto){
+            sum+=nos;
+            nos++;
+        }
+        System.out.println(sum);
+    }
+}
