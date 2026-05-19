2aab429100d3abbb395cd378a05c1d374bf791c1
john <johnludus1977@example.com>
2026-05-19 18:19:58 +0530

    very easy(without loop)

---
+++ src/SumOfNatural03.java
@@ -0,0 +1,7 @@
+public class SumOfNatural03 {
+    public static void main(String[] args){
+        int num=50;
+        int sum = num*(num+1)/2;
+        System.out.println("Sum of "+num+" natural numbers = " +sum);
+    }
+}
