f0c4bb6bc634cedb89f4483ad4b59ebceffd2d10
john <johnludus1977@example.com>
2026-05-21 10:45:23 +0530

    very easy(using for loop)

---
+++ src/UptoN.java
@@ -0,0 +1,9 @@
+public class UptoN {
+    public static void main(String[] args){
+        int check=20,sum=0;
+        for(int i=1;i<=check;i++){
+            sum+=i;
+            System.out.print(sum+" ");
+        }
+    }
+}
