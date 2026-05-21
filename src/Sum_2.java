58a2fc963215456f7c82d42432595ea0e0eafefb
john <johnludus1977@example.com>
2026-05-21 10:03:10 +0530

    very easy(using do while loop)

---
+++ src/Sum_2.java
@@ -0,0 +1,9 @@
+public class Sum_2 {
+    public static void main(String[] args){
+        int n=3,upto=20;
+        do{
+            System.out.print(n+" ");
+            n=n+2;
+        }while(n<=upto);
+    }
+}
