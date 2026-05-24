e83ec82785e3ade53ffc6554b370442e0a37129e
john <johnludus1977@example.com>
2026-05-24 22:24:20 +0530

    very easy(PTN)

--- src/StarPtn_02.java
+++ src/StarPtn_02.java
@@ -2,7 +2,7 @@ public class StarPtn_02 {
     public static void main(String[] args){
         int n=5;
         for(int i=0;i<n;i++){
-            for(int j=0;j<n-i;j++){
+            for(int j=n-i;j>=1;j--){
                 System.out.print('*');
             }
             System.out.println();
