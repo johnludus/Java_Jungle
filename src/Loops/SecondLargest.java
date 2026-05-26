a5d3e7d3b9f19f98c26b2fd2e86ef2b3d7440f0c
john <johnludus1977@example.com>
2026-05-26 12:55:24 +0530

    very easy

---
+++ src/Loops/SecondLargest.java
@@ -0,0 +1,20 @@
+package Loops;
+
+public class SecondLargest {
+    public static void main(String[] args){
+        int [] array = { 12,43,6,43,90,23,57};
+        int largest = 0;
+        int secondLargest = -1;
+        for(int i=0;i<array.length;i++){
+            if(array[i]>largest){
+                largest = array[i];
+            }
+        }
+        for(int i=0;i<array.length;i++){
+            if(array[i]>secondLargest && array[i]!=largest){
+                secondLargest = array[i];
+            }
+        }
+        System.out.println("Second Largest number : "+secondLargest);
+    }
+}
---
+++ src/Loops/SecondLargestTwo.java
@@ -0,0 +1,20 @@
+package Loops;
+
+public class SecondLargestTwo {
+    public static void main(String[] args){
+        int []array = {12,43,65,3,3,2,54};
+        int largest = -1;
+        int secondLargest = -1;
+        for(int i=0;i<array.length;i++){
+            if(array[i]>largest){
+                secondLargest = largest;
+                largest = array[i];
+
+            }
+            else if(array[i]>secondLargest && array[i]<largest){
+                secondLargest = array[i];
+            }
+        }
+        System.out.println(secondLargest);
+    }
+}
