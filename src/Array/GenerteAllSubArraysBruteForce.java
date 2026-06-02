d16918e58f7b2c887c964510e73676ec9ae0b35b
john <johnludus1977@example.com>
2026-06-02 17:02:44 +0530

    Very easy(Brute Force)

--- src/Array/GenerteAllSubArraysBruteForce.java
+++ src/Array/GenerteAllSubArraysBruteForce.java
@@ -2,7 +2,7 @@ package Array;
 
 import java.util.Scanner;
 
-public class GenerteAllSubArrays {
+public class GenerteAllSubArraysBruteForce {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of sn array : ");
