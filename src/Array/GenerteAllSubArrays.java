bf97d355626f76cd8c668d0900d38d982802a9d1
john <johnludus1977@example.com>
2026-06-02 16:57:57 +0530

    Very easy(Brute Force)

---
+++ src/Array/GenerteAllSubArrays.java
@@ -0,0 +1,26 @@
+package Array;
+
+import java.util.Scanner;
+
+public class GenerteAllSubArrays {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of sn array : ");
+        int size = sc.nextInt();
+        int[] array = new int[size];
+        System.out.println("Enter the size of an array : ");
+        for (int i=0;i<size;i++){
+            array[i] = sc.nextInt();
+        }
+        System.out.println("All subarrays Are : ");
+        for (int start = 0;start<size;start++){
+            for(int end = start;end<size;end++){
+                for(int i=start;i<=end;i++){
+                    System.out.print(array[i]);
+                }
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
